559a561b3aef69ad8c2f9c310030225a015eeacc
AnderPro <fabiander_21@hotmail.es>
2019-11-22 18:08:10 -0500

    angular componentes

---
+++ 03-Angular/componentes/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiPrimerComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
