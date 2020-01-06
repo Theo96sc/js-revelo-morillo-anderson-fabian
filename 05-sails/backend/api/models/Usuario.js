a3c80e5fe5936a0db318197768c4728c6233f598
andersonrev <anderson.revelo@epn.edu.ec>
2020-01-06 17:54:21 -0500

    backend

---
+++ 05-sails/backend/api/models/Usuario.js
@@ -0,0 +1,24 @@
+/**
+ * Usuario.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombres: {
+      type: 'string'
+    },
+    apellido: {
+      type: 'string'
+    },
+    password:{
+      type: 'string'
+    }
+
+  },
+
+};
+
