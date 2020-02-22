cae31987c9ef24994fe9a6697e6b398e6869a32b
Theo96sc <theo_96.sc@outlook.es>
2020-02-22 01:05:08 -0500

    aumente menu con botones

--- ProyectoJS/frontend-project/src/app/app.module.ts
+++ ProyectoJS/frontend-project/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import {ListarLogHistorialComponent} from './Modulos/logHistorial/listar-log-historial/listar-log-historial.component';
 import {FormsModule} from '@angular/forms';
-import {ButtonModule, InputTextModule, TableModule} from 'primeng';
+import {ButtonModule, InputTextModule, PanelModule, TableModule} from 'primeng';
 import {LogHistorialRestService} from './Servicios/LogHistorial.rest.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material';
@@ -15,6 +15,8 @@ import {WebsocketService} from './Servicios/websocket.service';
 import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { ListarAreaSensorComponent } from './Modulos/AreaSensor/listar-area-sensor/listar-area-sensor.component';
 import {AreaSensoresRestService} from './Servicios/areaSensores.rest.service';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
@@ -36,7 +38,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     MatFormFieldModule,
     HttpClientModule,
     MatInputModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    PanelModule
   ],
   providers: [
     LogHistorialRestService,
